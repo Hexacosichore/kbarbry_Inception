d18dad6a354734ea9f3cd91c938d840ab8147414
Hexacosichore <93528473+Hexacosichore@users.noreply.github.com>
2022-05-13 02:28:36 +0200

    lala

--- srcs/requirements/wordpress/conf/start.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-export $(cat .env | egrep -v "(^#.*|^$)" | xargs)
-
-cp wp-config.php /var/www/wordpress
-cp www.conf /etc/php/7.3/fpm/pool.d/www.conf
-
-service php7.3-fpm start
-service php7.3-fpm stop
-php-fpm7.3 -F -R
