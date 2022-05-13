5d98527093120c15b2d1671201f3ae3844c5e68a
Hexacosichore <93528473+Hexacosichore@users.noreply.github.com>
2022-05-13 02:26:54 +0200

    lala

--- srcs/requirements/mariadb/conf/start.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-mysql -e "CREATE DATABASE {DB_NAME}";
-mysql -e "CREATE USER '{USER_NAME_SQL}'@'%' IDENTIFIED BY '{USER_PSSWD_SQL}'";
-mysql -e "GRANT ALL PRIVILEGES ON {DB_NAME}.* TO '{USER_NAME_SQL}'@'%'";
-mysql -e "FLUSH PRIVILEGES";
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFiED BY 'ROOT_PSSWD_SQL'";
-
-exec mysqld
